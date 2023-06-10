aa122ec25cdafe06caa061fcec06681b403dd4e9
MariaRipley <roblesb.maria@gmail.com>
2023-06-10 20:36:09 +0200

    Get characters id

--- src/js/main.js
+++ src/js/main.js
@@ -20,6 +20,16 @@ function renderCharacterList(listData) {
   for (const character of listData) {
     ulElement.innerHTML += renderCharacter(character);
   }
+  addEventCharacter();
+}
+
+//Función para evento click sobre la tarjeta
+
+function addEventCharacter() {
+  const liElementList = document.querySelectorAll('.js_li_card');
+  for(const li of liElementList) {
+    li.addEventListener('click', handleClick);
+  }
 }
 
 //Función para generar una tarjeta
@@ -27,17 +37,24 @@ function renderCharacterList(listData) {
 function renderCharacter(character) {
   const valueImg = character.imageUrl;
   const valueName = character.name;
-  let html = `<li class="card"><div class="">
+  const valueId = character._id;
+  let html = `<li id="${valueId}" class="card js_li_card"><div class="">
         <img src="${valueImg}" alt="" class="card__img" />
         <p class="card__text">${valueName}</p>
         </div></li>`;
 
   if (!valueImg) {
     const blankImg = 'https://via.placeholder.com/210x295/ffffff/555555/?text=Disney';
-    html = `<li class="card"><div class="">
+    html = `<li id="${valueId}" class=""card js_li_card""><div class="">
             <img src="${blankImg}" alt="" class="card__img" />
             <p class="card__text">${valueName}</p>
             </div></li>`;
   }
   return html;
 }
+
+function handleClick(event) {
+  const id = event.currentTarget.id;
+  console.log(id);
+}
+
