c2324593bcdec6539929b5d700b00da14952ca83
MariaRipley <roblesb.maria@gmail.com>
2023-06-10 19:51:42 +0200

    Create list of characters and addapt some styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-console.log('>> Ready :)');
+const ulElement = document.querySelector('.js_character_list');
+
+const url = 'https://api.disneyapi.dev/character?pageSize=50';
+
+let listCharactersApi = [];
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    listCharactersApi = data.data;
+    renderCharacterList(listCharactersApi);
+  });
+
+//Función para generar listado de tarjetas
+
+function renderCharacterList(listData) {
+  for(const character of listData) {
+    ulElement.innerHTML += renderCharacter(character);
+  }
+}
+
+//Función para generar una tarjeta
+
+function renderCharacter(character){
+  let html = `<li class="card"><div class="">
+                    <img src="${character.imageUrl}" alt="" class="card__img" />
+                    <p class="card__text">${character.name}</p>
+              </div></li>`;
+  const valueImg = character.imageUrl;
+  const blankImg = 'https://via.placeholder.com/210x295/ffffff/555555/?text=Disney';
+  if(valueImg === '') {
+    html = `<li class="card"><div class="">
+                    <img src="${blankImg}" alt="" class="card__img" />
+                    <p class="card__text">${character.name}</p>
+              </div></li>`;
+  }
+  return html;
+}
+
+
